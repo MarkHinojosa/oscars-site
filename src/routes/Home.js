696db5b8b17faf49a421d848e38c477dfaca85a1
mark <markhinojosa1212@gmail.com>
2021-03-10 02:13:02 -0600

    added video, layout of home screen

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,39 @@
+import logo from "../assets/medEscort.png";
+import clip from "../assets/droneFootage.mp4";
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height,
+    };
+}
+
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(
+        getWindowDimensions()
+    );
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowDimensions;
+}
+
 export default function Home() {
+    const { height, width } = useWindowDimensions();
+
     return (
         <div
             style={{
-                backgroundColor: "#071F4E",
+                backgroundColor: "white",
                 color: "white",
                 height: "100%",
                 flex: 1,
@@ -13,12 +44,12 @@ export default function Home() {
         >
             <div
                 style={{
-                    height: "30%",
+                    height: "20%",
                     width: "100%",
-                    backgroundColor: "green",
+                    backgroundColor: "#071F4E",
                     flex: 1,
                     display: "flex",
-                    justifyContent: "center",
+                    // justifyContent: "center",
                     flexDirection: "column",
                     alignItems: "center",
                     // alignContent: "center",
@@ -27,9 +58,9 @@ export default function Home() {
             >
                 <div
                     style={{
-                        backgroundColor: "red",
-                        width: "30%",
-                        height: "100%",
+                        // backgroundColor: "red",
+                        width: "50%",
+                        // height: "100%",
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
@@ -42,20 +73,44 @@ export default function Home() {
                             width: 10,
                         }}
                     ></div> */}
-                    <h2
-                        style={{
-                            textAlign: "center",
-                            fontSize: 28,
-                            fontWeight: "bold",
-                            fontFamily: "fantasy",
-                        }}
-                    >
-                        HHIT
-                    </h2>
+                    <img style={{ height: "100%", width: "100%" }} src={logo} />
                 </div>
-                <text style={{ fontFamily: "cursive", fontSize: 22 }}>
+                <text style={{ fontFamily: "cursive", fontSize: 20 }}>
                     Hispanic Healthcare Initiative of Texas
                 </text>
+            </div>
+            <div>
+                <video
+                    id="video"
+                    // className="videoTag"
+                    autoPlay
+                    loop
+                    muted
+                    // style={{
+                    //     backgroundColor: "red",
+                    //     height: "100%",
+                    //     width: "100%",
+                    // }}
+                >
+                    <source
+                        // className="videoTag"
+                        // style={{ height: { height } - "500px" }}
+                        src={clip}
+                        type="video/mp4"
+                    ></source>
+                </video>
+                {/* <div id="leftSection"></div> */}
+            </div>
+            <div
+                style={{
+                    width: "100%",
+                    backgroundColor: "yellow",
+                    flex: 1,
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <text style={{ color: "green" }}>Footer</text>
             </div>
         </div>
     );
